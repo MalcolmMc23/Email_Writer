3998580b64c3822ff5d2cb1682f0037f7818b16b
Malcolm McDonald <malcolmemcdonald@gmail.com>
2023-11-17 09:13:34 -0800

    finished chatgpt function calling
    if you prompt it to send an email to an email it will send a prewriten email to the one you gave it.

--- app.js
+++ app.js
@@ -1,7 +1,56 @@
-const nodemailer = require('nodemailer');
-const { google } = require('googleapis');
-const { config } = require("dotenv");
+
+import nodemailer from "nodemailer"
+import { google } from "googleapis"
+import { config } from "dotenv"
 config()
+import { OpenAI } from "openai";
+
+const openai = new OpenAI();
+
+function sendMailTest(appendString) {
+    console.log("Sending mail to " + appendString);
+}
+
+async function main(appendString) {
+    const messages = [{ "role": "user", "content": "send an email to malcolmemcdonald@gmail.com" }];
+    const response = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: messages,
+        tools: [{
+            "type": "function",
+            "function": {
+                "name": "sendMail",
+                "description": "sends an email to the a email adress ",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "ToMail": {
+                            "type": "string",
+                            "description": "The Email your sending to",
+                        }
+                    },
+                    "required": ["ToMail"]
+                },
+            }
+        }],
+        tool_choice: "auto",  // auto is default, but we'll be explicit
+    });
+
+
+    let wantsToUseFunction = response.choices[0].finish_reason == "tool_calls"
+    if (wantsToUseFunction) {
+        if (response.choices[0].message.tool_calls[0].function.name == "sendMail") {
+            let argumentObj = JSON.parse(response.choices[0].message.tool_calls[0].function.arguments);
+            sendMail(argumentObj.ToMail)
+        }
+    }
+}
+main("its about TIME!")
+
+
+
+
+
 
 
 const CLIENT_ID = process.env.CLIENT_ID
@@ -12,7 +61,7 @@ const REDIRECT_URI = 'https://developers.google.com/oauthplayground'
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
 oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN })
 
-async function sendMail() {
+async function sendMail(ToMail) {
     try {
         const accessToken = await oAuth2Client.getAccessToken()
 
@@ -30,22 +79,20 @@ async function sendMail() {
 
         const mailOptions = {
             from: 'Malcolm McDonald <malcolm.e.mcdonald@gmail.com>',
-            to: '810828@seq.org',
+            to: ToMail,
             subject: "Hello from gmail using api",
-            text: "",
+            text: "Hello From ChatGPT's api",
         }
 
-        console.log("%%%%%%%%%%%%%%%%%%%%", mailOptions)
 
 
         const result = await transport.sendMail(mailOptions)
 
-
+        console.log(result)
         return result;
     } catch (error) {
         return error
     }
 }
 
-
-sendMail().then(result => console.log("Email Sent!!!", result)).catch(error => console.log(error.message))
+    // sendMail().then(result => console.log("Email Sent!!!", result)).catch(error => console.log(error.message))
